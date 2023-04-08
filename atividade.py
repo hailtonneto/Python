cedabfb64e185b962a62f96fd6f6cd769f6ef4e1
Hailton Neto <130097508+hailtonneto@users.noreply.github.com>
2023-04-08 10:45:26 -0300

    python

---
+++ atividade.py
@@ -0,0 +1,9 @@
+#da a idade de três crianças e tem que descobrir a média da idade delas
+
+idade1 = int(input("Digite a idade da pessoa 1:"))
+idade2 = int(input("Digite a idade da pessoa 2:"))
+idade3 = int(input("Digite a idade da pessoa 3:"))
+
+media = (idade1+idade2+idade3)/3
+
+print(media)
