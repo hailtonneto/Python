dff75f9a165c74471c957233e34a7ec9c46ff697
Hailton Neto <130097508+hailtonneto@users.noreply.github.com>
2023-04-08 01:01:53 -0300

    python

---
+++ aula1.py
@@ -0,0 +1,29 @@
+# print() ; mostrar algo na tela
+
+print("hello world")
+print("hello"+"world")
+print("h""e""l""l""o"" ""w""o""r""l""d")
+
+#operações aritmeticas + soma, - subtração, / divisão, * Mutiplicação, ** Exponenciação, // numero da divisão inteira, % resto da divisão
+
+print(2+2)
+print(3-1)
+print(8/2)
+print(2*4)
+print(2**2)
+print(15//4)
+print(7%3)
+
+#transformação de tipos de caracteres int (número inteiro sem vírgula), float (números irracionais), string (é um tipo de dados usado para armazenar informações de texto)
+
+print(int(123.3)) 
+print(float(120.8))
+print(str(234+23))
+
+a = str(2+3)
+print(type(a))
+
+#funções len (ler a quantidade de caracteres de um string)
+
+print(len("eu sou chat gpt um modelo de linguagem desnevolvido pelo openai"))
+print(len(str(242242424242424)))
